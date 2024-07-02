09a5a248e05de288f3beb6eac2e33f46c28e62cc
changlabntu <82998390+changlabntu@users.noreply.github.com>
2024-07-02 18:45:34 +0800

    FIX IsoSceopXY AGAIN

--- models/IsoScopeXY.py
+++ models/IsoScopeXY.py
@@ -91,10 +91,10 @@ class GAN(BaseModel):
         # Finally, initialize the optimizers and scheduler
         self.configure_optimizers()
 
-        if self.hparams.cropz > 0:
-            self.upsample = torch.nn.Upsample(size=(hparams.cropsize, hparams.cropsize, hparams.cropz * hparams.uprate), mode='trilinear')
-        else:
-            self.upsample = torch.nn.Upsample(size=(hparams.cropsize, hparams.cropsize, 32 * hparams.uprate), mode='trilinear')
+        self.upsample = torch.nn.Upsample(size=(hparams.cropsize, hparams.cropsize, hparams.cropsize), mode='trilinear')
+
+        self.uprate = (hparams.cropsize // hparams.cropz)
+        print('uprate: ' + str(self.uprate))
 
         # CUT NCE
         if not self.hparams.nocut:
@@ -120,7 +120,6 @@ class GAN(BaseModel):
         parser = parent_parser.add_argument_group("LitModel")
         # coefficient for the identify loss
         parser.add_argument("--lambI", type=int, default=0.5)
-        parser.add_argument("--uprate", type=int, default=4)
         parser.add_argument("--skipl1", type=int, default=1)
         parser.add_argument("--nocyc", action='store_true')
         parser.add_argument("--nocut", action='store_true')
@@ -133,6 +132,7 @@ class GAN(BaseModel):
         parser.add_argument('--use_mlp', action='store_true')
         parser.add_argument("--c_mlp", dest='c_mlp', type=int, default=256, help='channel of mlp')
         parser.add_argument('--fWhich', nargs='+', help='which layers to have NCE loss', type=int, default=None)
+        parser.add_argument('--transpose', action='store_true', help='transpose to the yz plane')
         return parent_parser
 
     def test_method(self, net_g, img):
@@ -144,13 +144,13 @@ class GAN(BaseModel):
         if self.hparams.cropz > 0:
             z_init = np.random.randint(batch['img'][0].shape[4] - self.hparams.cropz)
             batch['img'][0] = batch['img'][0][:, :, :, :, z_init:z_init + self.hparams.cropz]
-        #batch['img'][1] = batch['img'][1][:, :, :, :, z_init:z_init + self.hparams.cropz]
 
         self.oriX = batch['img'][0]  # (B, C, X, Y, Z) # original
-        #self.oriY = batch['img'][1]  # (B, C, X, Y, Z) # original
 
         self.Xup = self.upsample(self.oriX)  # (B, C, X, Y, Z)
-        #self.Yup = self.upsample(self.oriY)  # (B, C, X, Y, Z)
+
+        if self.hparams.transpose:
+            self.Xup = self.Xup.permute(0, 1, 3, 4, 2)  # (B, C, Y, Z, X)
 
         self.goutz = self.net_g(self.Xup, method='encode')
         self.XupX = self.net_g(self.goutz[-1], method='decode')['out0']
@@ -158,8 +158,8 @@ class GAN(BaseModel):
         if not self.hparams.nocyc:
             self.XupXback = self.net_gback(self.XupX)['out0']
 
-    def get_xy_plane(self, x):  # (B, C, X, Y, Z)
-        return x.permute(4, 1, 2, 3, 0)[::1, :, :, :, 0]  # (Z, C, X, Y, B)
+    def get_xy_plane(self, x):
+        return x.permute(4, 1, 2, 3, 0)[::1, :, :, :, 0]
 
     def adv_loss_six_way(self, x, net_d, truth):
         loss = 0
@@ -243,7 +243,7 @@ class GAN(BaseModel):
 
         dxx = self.adv_loss_six_way(self.XupX, net_d=self.net_d, truth=False)
         # ADV(X)+
-        dx = self.add_loss_adv(a=self.get_xy_plane(self.oriX), net_d=self.net_d, truth=True)
+        dx = self.add_loss_adv(a=self.get_xy_plane(self.XupX[:, :, :, :, ::self.uprate]), net_d=self.net_d, truth=True)
 
         loss_dict['dxx_x'] = dxx + dx
         loss_d += dxx + dx
@@ -262,4 +262,4 @@ class GAN(BaseModel):
 
 
 # USAGE
-# python train.py --jsn cyc_imorphics --prj IsoScope0/0 --models IsoScope0 --cropz 16 --cropsize 128 --netG ed023d --env t09 --adv 1 --rotate --ngf 64 --direction xyori --nm 11 --dataset longdent
+# python train.py --jsn cyc_imorphics --prj IsoScopeXY/ngf32lb10 --models IsoScopeXY --cropz 20 --cropsize 128 --env runpod --adv 1 --rotate --ngf 32 --direction xyori --nm 00 --netG ed023d --dataset Fly0B --n_epochs 6000 --lr_policy cosine --mc --lamb 10 --skipl1 4
